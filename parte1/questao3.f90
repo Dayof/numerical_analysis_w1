1958530220882975efc675a99cfb73e022bd0460
Yurickh <yurick.hauschild@gmail.com>
2016-10-06 19:43:30 -0300

    Questão 4 feita

--- parte1/questao3.f90
+++ parte1/questao3.f90
@@ -3,25 +3,27 @@ program questao3
     implicit none
 
     integer :: j, n
-    real :: lambda=3, x, xn
+    real :: lambda=3.0, x, xn
     real, dimension(1000) :: xslist
     integer :: xslist_iter = 0
 
-    real, parameter :: precision = 0.0001
+    real, parameter :: precision = 0.01 ! sorry about that
 
     ! Write data in output files
     open(unit=1, file="data/questao3.dat", status="unknown", action="write")
+    open(unit=2, file="data/questao4.dat", status="unknown", action="write")
 
         do j=3000, 4000
             lambda = lambda + 0.001
-            xn = 0.2
-            call resetIterator(xslist_iter)
+            xn = 0.4
+            call resetList(xslist, xslist_iter)
 
-            do n=1, 1000 ! infinite loop
+            do n=1, 1000 ! pseudo-infinite loop
                 x = xn
-                xn = f(x, lambda)
+                xn = f(x, j/1000.0)
 
-                if(isInside(xslist, xslist_iter, xn).eq.0) then
+                if(isInside(xslist, xslist_iter, xn) .ne. 0) then
+                    write(2, *) j/1000.0, xslist_iter-1
                     exit ! breaks loop if we find a cicle
                 else
                     ! add xn to end of list
@@ -34,6 +36,7 @@ program questao3
         end do
 
     close(unit=1)
+    close(unit=2)
 
 contains
 
@@ -56,32 +59,34 @@ contains
         real :: element
         integer :: isInside, iter
 
-        integer :: i
-        integer :: found=0
+        integer :: i=1
+
+        isInside = 0
 
         do i=1, iter
-            if(list(i) - element .lt. precision ) then ! if list(i) == element
-                found = i
+            if(abs(list(i) - element) .lt. precision ) then
+            ! if list(i) ~ element
+                isInside = i
                 exit
             end if
         end do
-
-        isInside = found
 
         return
 
     end function isInside
 
-    subroutine resetIterator(iterator)
+    subroutine resetList(list, iterator)
 
         implicit none
 
         integer, intent(in out) :: iterator
+        real, dimension(1000), intent(in out) :: list
 
         iterator = 1
+        list = 0
 
         return
-    end subroutine resetIterator
+    end subroutine resetList
 
     subroutine addToList(list, iterator, element)
 
@@ -93,8 +98,6 @@ contains
 
         list(iterator) = element
         iterator = iterator + 1
-
-        write(*,*) iterator
 
         return
     end subroutine addToList
