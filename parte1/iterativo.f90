babb576238f913057403f8c1a2c634e56eaf1758
Day <dayannefernandesc@gmail.com>
2016-10-06 01:12:08 -0300

    Removing warning in loop from part1 and add gitignore

--- parte1/iterativo.f90
+++ parte1/iterativo.f90
@@ -2,23 +2,25 @@ program iterativo
 
     implicit none
 
-    double precision :: lambda, x=100, xn=0.2
+    integer :: j
+    real :: lambda, x, xn
+    real, dimension (6) :: lambda_values
 
-    read(*,*) lambda
+    lambda_values = (/ 0.4, 0.9, 1.4, 1.9, 2.4, 2.9 /)
 
     ! Write data in output file
     open(unit=1, file="iterativo.dat", status="unknown", action="write")
 
-        do lambda=0.4, 3, 0.5
-            x = 100
+        do j=1, size(lambda_values)
+            x = 100.0
             xn = 0.2
 
             do while(abs(xn - x) .gt. 0.0000001)
                 x = xn
-                xn = f(x, lambda)
+                xn = f(x, lambda_values(j))
             end do
 
-            write(1, *) x, lambda
+            write(1, *) x, lambda_values(j)
 
         end do
 
@@ -30,7 +32,7 @@ contains
 
         implicit none
 
-        double precision :: f, x, l
+        real :: f, x, l
 
         f = l * (x - x*x)
 
