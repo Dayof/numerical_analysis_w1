701e88d5065ed82e17251a1cc2ce8c32f0efe8f0
Yurickh <yurick.hauschild@gmail.com>
2016-10-06 16:11:41 -0300

    Expandindo os dados para os gráficos.

--- parte1/iterativo.f90
+++ parte1/iterativo.f90
@@ -2,29 +2,40 @@ program iterativo
 
     implicit none
 
-    integer :: j
+    integer :: j, n
     real :: lambda, x, xn
     real, dimension (6) :: lambda_values
 
     lambda_values = (/ 0.4, 0.9, 1.4, 1.9, 2.4, 2.9 /)
 
-    ! Write data in output file
-    open(unit=1, file="iterativo.dat", status="unknown", action="write")
+    ! Write data in output files
+    open(unit=1, file="data/questao2.1.dat", status="unknown", action="write")
+    open(unit=2, file="data/questao2.2.dat", status="unknown", action="write")
+    open(unit=3, file="data/questao2.3.dat", status="unknown", action="write")
+    open(unit=4, file="data/questao2.4.dat", status="unknown", action="write")
+    open(unit=5, file="data/questao2.5.dat", status="unknown", action="write")
+    open(unit=6, file="data/questao2.6.dat", status="unknown", action="write")
 
         do j=1, size(lambda_values)
             x = 100.0
             xn = 0.2
+            n = 0
 
-            do while(abs(xn - x) .gt. 0.0000001)
+            do while(abs(xn - x) .gt. 0.0001)
+                n = n+1
                 x = xn
                 xn = f(x, lambda_values(j))
+                write(j, *) n, xn
             end do
-
-            write(1, *) x, lambda_values(j)
 
         end do
 
     close(unit=1)
+    close(unit=2)
+    close(unit=3)
+    close(unit=4)
+    close(unit=5)
+    close(unit=6)
 
 contains
 
